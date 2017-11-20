9d913a57e0eeb4f137087554c3097a07be9b65eb
唐成 <tangcheng@hujiang.com>
2017-11-20 20:07:43 +0800

    function val(Value)不可变引用，类的只读属性.java中的final var(Variable)可变引用，类的可读，可写属性.java中的非final 语句和表达式的区别在于，表达式有值，并且能作为另一个表达式的一部分使用；类比正则表达式 而语句总是包围着它的代码块中的顶层元素，并且没有自己的值。 在Kotlin中if是表达式，而不是语句。即可以像return a+b一样使用一个return,也不需要使用function封装 在Kotlin中除了循环(for,do和do/while)以外大多数控制结构都是表达式。这种结合控制结构和其它表达式的能力可以简明扼要地表示许多常见的模式。 在Java中，所有的控制结构都是表达式。要取得每种情况的值，需要使用多个return，或使用function封装
    Java中的赋值操作是表达式，Kotlin中赋值操作是语句

--- src/com/tangcheng/kotlin/study/HelloWorld.kt
+++ src/com/tangcheng/kotlin/study/HelloWorld.kt
@@ -7,6 +7,13 @@ package com.tangcheng.kotlin.study
  */
 
 fun main(args: Array<String>) {
+    if (args.isNotEmpty()) {
+        println("Hello,${args[0]}")
+    }
+    val name = if (args.isNotEmpty()) args[0] else "Kotlin"
+    println("Hello,$name!")
+    println("Hello,${if (args.isNotEmpty()) args[0] else "Kotlin"}!22")
+
     println("hello,world!")
 
     val persons = listOf<Person>(Person("Alice"), Person("Bob", age = 29))
@@ -14,7 +21,17 @@ fun main(args: Array<String>) {
     println("The oldest is : $oldest")
     val smallest = persons.minBy { it.age ?: 0 }
     println("The smallest is : $smallest")
+
+    println("10,20 max:${maxStyle1(10, 20)}")
+    println("20,30 max:${maxStyle2(20, 30)}")
 }
 
 data class Person(val name: String,
                   val age: Int? = null)
+
+fun maxStyle1(a: Int, b: Int): Int {
+    return if (a > b) a else b
+}
+
+fun maxStyle2(a: Int, b: Int): Int = if (a > b) a else b
+
